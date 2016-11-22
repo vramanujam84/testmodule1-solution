301cdea26ae8710e895529ca3bc3e0be7ee8578d
Priya <vishnupriya84@gmail.com>
2016-11-22 00:21:07 -0500

    module 1 done

--- app.js
+++ app.js
@@ -4,11 +4,27 @@
 angular.module('module1-solution', [])
 
 .controller('module1-controller', function ($scope) {
-$scope.name ="Priya";
+$scope.name ="";
 $scope.text="";
 
-$scope.sayHello = function(){
-  $scope.text = "Hello!";
+$scope.foodFinder = function(){
+  var array = splitString($scope.name);
+  var len = array.length;
+  if(len>1 && len<=3){
+  $scope.text = "Enjoy!";
+}
+  else  if (len>3) {
+      $scope.text = "Too much";
+  }
+  else if (len==0) {
+    $scope.text = "Enter data.";
+  }
+}
+
+function splitString(string)
+{
+  var array = string.split(",");
+  return array;
 }
 
 $scope.clear = function(){
